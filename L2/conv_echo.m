f7d8741e8c6a4e480c6858093fc64632f1568928
gershkoj <gershkoj@mcmaster.ca>
2023-10-25 23:00:34 -0400

    two echo functions

--- L2/conv_echo.m
+++ L2/conv_echo.m
@@ -3,14 +3,16 @@ L = length(signal); % Number of samples in the signal.
 T = 1/Fs; % Sampling period in seconds.
 t = [0:L-1]*T; % Time vector in seconds.
 Te = 2; % delay in seconds.
-A = 0.3 % Amplitude factor
+A = 0.3; % Amplitude factor
 
-Le = Te*Fs; % extra samples needed to finish echo
+Le = Te*Fs; % Delay in samples until impulse
 
-padding = zeros(Le, 1);
+impulse = zeros(1, Le);
+impulse(Le) = A;
+echosignal = conv(signal, impulse);
 
-signalplusecho =  [signal ; padding] + A.*[padding ; signal];
+signalplusecho = [signal ; zeros(Le-1,1)] + echosignal;
 
 signalplusecho = signalplusecho / max(abs(signalplusecho));
 
-audiowrite('speechwithecho.wav', signalplusecho, Fs);
+audiowrite('speechwithecho_conv.wav', signalplusecho, Fs);
